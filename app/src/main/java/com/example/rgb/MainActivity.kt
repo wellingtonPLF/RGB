2fc03176704bbde8d4c1f3dbd9b590a485da5362
Wellington Pessoa de Lima Filho <wellplf@gmail.com>
2021-11-25 09:08:37 -0300

    'Review'

--- app/src/main/java/com/example/rgb/MainActivity.kt
+++ app/src/main/java/com/example/rgb/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         this.bt1 = findViewById(R.id.bt1)
         this.bt1.setOnClickListener{
-            val intent: Intent = Intent(Intent.ACTION_SEND)
+            val intent: Intent = Intent(this, ReceiverScreen::class.java)
             intent.setType("text/plain")
             intent.putExtra(Intent.EXTRA_TEXT, "12, 205, 125")
             startActivity(intent)
